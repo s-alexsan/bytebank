4e16dbef9ed9a742db3d1461fd39199c6710f274
Alexsander da Silva <alexsans@outlook.com.br>
2022-05-11 22:13:35 -0300

    add form

---
+++ src/app/novaTransferencia.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-nova-tranferencia',
+    templateUrl:'./html/novaTransferencia.component.html',
+    styleUrls: ['./css/novaTranferencia.component.css']
+})
+
+export class NovaTransferenciaComponent{
+
+}
